dd2b374e388a8ef4012fe7e680347ea0585df958
yagur <yagur@DESKTOP-HJ1O55H>
2025-07-24 18:46:35 +0300

    DOM

---
+++ 02-JavaScript/03-DOM/DOM.js
@@ -0,0 +1,10 @@
+﻿// JavaScript source code
+let check = document.getElementById("check");
+check.innerHTML = "Hello DOM";
+console.log(check.outerHTML);
+
+let confinguration = document.getElementById("configuration");
+confinguration.append("append();");
+confinguration.prepend("prepend();");
+confinguration.before("h2 добавление элементов к ноде h2;");
+confinguration.after("end");
